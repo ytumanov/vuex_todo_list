f00e18160b7f92f04edb40f8adeff53800db5efa
Yaroslav Tumanov <airhole.ua@gmail.com>
2019-11-18 17:18:54 +0200

    todo list with vuex

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+//VUEX entry point
+import Vuex from "vuex";
+import Vue from "vue";
+import todos from "./modules/todos";
+
+// Load Vuex
+Vue.use(Vuex);
+
+// Create store
+export default new Vuex.Store({
+  modules: {
+    todos
+  }
+});
